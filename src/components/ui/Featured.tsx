b66dbfc0ecd469b01873e9a1f1d8f080daa5d4ff
Saif Maamun <sadifmamun@gmail.com>
2024-05-19 15:04:33 +0600

    vercel

--- src/components/ui/Featured.tsx
+++ src/components/ui/Featured.tsx
@@ -1,6 +1,7 @@
 import { ProductCardProps } from "../../types/productCardProps";
 
 const Featured: React.FC<ProductCardProps> = ({ product }) => {
+  console.log(product);
   return <div></div>;
 };
 
